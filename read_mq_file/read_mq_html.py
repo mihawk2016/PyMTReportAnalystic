db38db16bb7a09c16afdf9d73f5659219f641c74
mihawk <mihawk20101@sina.com>
2016-12-07 01:12:19 +0800

    edit at home

--- read_mq_file/read_mq_html.py
+++ read_mq_file/read_mq_html.py
@@ -39,9 +39,9 @@ def html_content(html_file):
     return mq_html_content
 
 
-def html_table(html_file):
+# def html_table(html_file):
     # print(len(pd.read_html(html_file, encoding='UTF-8', index_col=False)))
-    return pd.read_html(html_file, encoding='UTF-8', na_values='', keep_default_na=False)[0]
+    # return pd.read_html(html_file, encoding='UTF-8', na_values=' ', keep_default_na=False)[0]
 
 
 def read_html_mt4(html_content):
@@ -60,9 +60,48 @@ def read_html_mt4(html_content):
 
 
 def read_html_mt4_trade(html_file, html_content):
+    table = pd.read_html(html_file, encoding='UTF-8', na_values=' ', keep_default_na=False)[0]
+    summary_index = list(table[0]).index('Summary:')
+    table = table[:summary_index]
+    # table.columns = [['1', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2', '2']]
+    # print(table, '\n')
     broker = html_content.find('b').get_text()
-    tr = html_content.find_all('tr')
-    print(tr)
+    comment = [tr.td.get('title') if tr.td.has_attr('title') else ''
+               for tr in html_content.find_all('tr', limit=summary_index)]
+
+    # b = table.select(lambda x: list(x)[0].isdigit(), axis=1)
+    b = []
+
+    def get_group(table_row):
+        row_list = list(table_row)
+        if not row_list[0].isdigit():
+            return ''
+        leer_count = row_list.count('')
+        if leer_count is 0:
+            return 'Closed'
+        if leer_count is 9:
+            return 'Money'
+        if leer_count is 3:
+            return 'Pending'
+        if leer_count is 1:
+            return 'Open'
+        if leer_count is 5:
+            return 'Working'
+        return ''
+    table['Group'] = table.apply(get_group, axis=1)
+    # a = table.apply(get_group, axis=1)
+    # print(a)
+
+    table['Comment'] = comment
+
+    # for i in range(summary_index):
+    #     print(list(table.iloc[i]).count(''))
+
+    print(table)
+    print(b)
+    # for x in comment:
+    #
+    #     print(x)
     # table = html_table(html_file)
 
     pass
@@ -85,10 +124,17 @@ def build_info(account=None, name=None, currency=None, leverage=None, time=None,
 
 
 if __name__ == '__main__':
+    import timeit
     file = '../_TEST_FILE/MT4Trade.htm'
     content = html_content(file)
-    table = html_table(file)
+    # table = html_table(file)
+
     read_html_mt4_trade(file, content)
+
+    # def TEST():
+    #     return read_html_mt4_trade(file, content)
+    # print(timeit.timeit('TEST()', setup='from __main__ import TEST', number=10))
+
     # a = x.fillna('')
 
     # a = list(table.ix[0])
